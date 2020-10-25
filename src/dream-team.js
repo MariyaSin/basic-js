7eadd98806dc032f60bd13ae0493cf0be758bdf6
MariyaSin <maria1991@list.ru>
2020-10-25 15:46:47 +0300

    feat: dream-team and transform-array

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(array) {
+
+  if (!Array.isArray(array)){
+    return false;
+  }
+  let abc = array.reduce(function(x, item){
+    if (typeof(item) === 'string') {
+      let i = 0;
+      while (item[i] === ' ') {
+        i ++;
+      }
+      x += item[i];
+    }
+    return x;
+ }, '');
+ return abc.toUpperCase().split('').sort().join('');
 };
