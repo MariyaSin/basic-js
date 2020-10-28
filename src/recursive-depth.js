6a121642f00d0e70e618739179bc31711db0ec3c
MariyaSin <maria1991@list.ru>
2020-10-29 00:44:36 +0300

    feat: added extended-repeater function

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor () {
+    this.counter = 0;
+    this.deep = 0
   }
-};
+  
+  calculateDepth(arr) {
+    throw new CustomError('Not implemented');
+    // remove line with error and write your code here\
+    
+    /*for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] === 'object') {
+        this.deep += 1;
+        return this.calculateDepth(arr);
+      }
+    }
+    if (this.counter < this.deep) {
+      this.counter = this.deep;
+    };
+    this.deep = 0;
+    return this.counter;
+    }*/
+  }
+}
