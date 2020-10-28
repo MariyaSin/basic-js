5a280781400fd38eae4697df87e429de4bffa740
MariyaSin <maria1991@list.ru>
2020-10-28 05:46:41 +0300

    feat: added carbon-dating, countCat, hanoi-tower functions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,10 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+  let time = Math.log2(MODERN_ACTIVITY/sampleActivity) / (0.693/HALF_LIFE_PERIOD);
+  return (time > 0) && (time != Infinity) ? time: false;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,15 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(arr) {
-  throw new CustomError('Not implemented');
-  /*if (arr.length == 0){
+  if (arr.length == 0){
     return 0;
   }
   arr = arr.flat(Infinity);
-  return arr.reduce(function (s, elem){
-    if (typeof elem == String && elem == '^^'){
-      return s++;
-    }
-    return s;
-  }, 0)*/
+  return arr.reduce(function (s, elem) {
+    return (typeof elem == 'string' && elem == '^^' ? (s += 1): s);
+  }, 0)
 };
